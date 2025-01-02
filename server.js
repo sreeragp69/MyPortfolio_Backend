19744b513b3211d3c1c54b615fdf54038cfdf179
sreeragp69 <psreerag69@gmail.com>
2025-01-02 16:38:28 +0530

    Cors updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://myportfolio-kx1g.onrender.com",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 app.use(cors(corsOptions));
